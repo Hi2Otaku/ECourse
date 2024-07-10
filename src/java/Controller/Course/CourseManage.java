b7c6c7c1c236ee5c6b42d5632e208f65238d58c5
Hi2Otaku <hi2otaku@gmail.com>
2024-07-10 14:14:45 +0700

    Commit

--- src/java/Controller/Course/CourseManage.java
+++ src/java/Controller/Course/CourseManage.java
@@ -37,7 +37,7 @@ public class CourseManage extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        
+        doGet(request, response);
     }
 
 }
